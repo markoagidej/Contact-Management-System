c727589256145fc98e020a90734508e9471b0248
Marko <markogidej@yahoo.com>
2024-04-09 18:32:46 -0400

    simple 1. wokring and 5./validate email done

--- Contact Management System.py
+++ Contact Management System.py
@@ -6,30 +6,75 @@ contact_list = {}
 
 
 def add_contact():
-    pass
+    global contact_fields
+    global contact_list
+    print("Add new contact...")
+
+    contact_data = []
+    email = ""
+
+    for field in contact_fields:
+        if field == "Email":
+            while True:
+                email = input("Enter email address: ")
+                if validate_email(email):
+                    break
+        else:
+            field = input(f"Enter {field}: ")
+            if field == "/end":
+                while len(contact_data) < len(contact_fields) - 1:
+                    contact_data.append("")
+                break
+            else:
+                contact_data.append(field)
+
+    contact_list[email] = contact_data
+    print(f"Added contact for {email}: {contact_data}")
+    
+    update_backup()
 
 
 def edit_contact():
+    global contact_fields
+    global contact_list
     pass
 
 
-def deelte_contact():
+def delete_contact():
+    global contact_fields
+    global contact_list
     pass
 
 
 def search_contact():
+    global contact_fields
+    global contact_list
     pass
 
 
-def disaplys_contacts():
-    pass
+def dispaly_contacts():
+    global contact_fields
+    global contact_list
+    
+    for contact, details in contact_list.items():
+        details_string = ""
+        field_num = 1
+        for field_detail in details:
+            details_string += contact_fields[field_num] + " - " + field_detail + " | "
+            field_num += 1
+
+        print(f"{contact}: {details_string}")
 
 
 def export_contacts():
+    global contact_fields
+    global contact_list
     pass
 
 
 def import_contacts():
+    global contact_fields
+    global contact_list
     pass
 
 
@@ -41,8 +86,24 @@ def quit_app():
 def update_backup():
     pass
 
+def validate_email(email_string):
+    global contact_fields
+    global contact_list
+
+    if not re.search(r"[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}", email_string):
+        print("Must be a valid email format!")
+        return False
+
+    if email_string in contact_list.keys():
+        print(f"Contact with email of \'{email_string}\' already exists! Try using Edit instead.")
+        return False
+    
+    return True
 
 def main ():
+    global contact_fields
+    global contact_list
+
     try:
         with open("Files/contact_list.txt", "r") as file:
             lines = file.readlines()
@@ -83,11 +144,11 @@ def main ():
         elif choice == "2":
             edit_contact()
         elif choice == "3":
-            deelte_contact()
+            delete_contact()
         elif choice == "4":
             search_contact()
         elif choice == "5":
-            disaplys_contacts()
+            dispaly_contacts()
         elif choice == "6":
             export_contacts()
         elif choice == "7":
